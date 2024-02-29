e16b3bc5a34f50bdb8cace34c479d8ec96353de5
Rishabh Kumar <rsk1998200@gmail.com>
2024-02-29 21:12:33 +0530

    First 3 sum program in java

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static int addThreeNumbers(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int result = addThreeNumbers(1,2,3);
+        System.out.println(result);
+    }
+}
